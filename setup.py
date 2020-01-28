f6bc4e022785d298cb6a2fcbbfb9818c57998da7
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-01-28 18:14:40 -0500

    update version number

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ setup(
     include_package_data=True,
     classifiers=["Development Status :: 4 - Beta","Topic :: Software Development :: Libraries","Topic :: Software Development :: Libraries :: Python Modules","License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)","Programming Language :: Python :: 2.7","Programming Language :: Python :: 3.6","Programming Language :: Python :: 3.7","Programming Language :: Python :: 3.8"],
     install_requires=["fabric2","jx-python","mo-collections","mo-dots","mo-files","mo-future","mo-json","mo-kwargs","mo-logs","mo-math","mo-sql","mo-times","pyLibrary","pymysql"],
-    version=u'3.35.20028',
+    version=u'3.36.20028',
     url=u'https://github.com/klahnakoski/jx-bigquery',
     python_requires=u'==2.7, ==3.6, ==3.7, ==3.8',
-    packages=["jx_mysql"],
+    packages=["jx_mysql/expressions","jx_mysql"],
     long_description=u'# jx-mysql\nJSON query expressions for MySQL, and derivatives\n',
     name=u'jx-mysql'
 )
